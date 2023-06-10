75d0cc4dc42105a8126ab707ba294e7718b36b57
Mizanur Rahman <mizanrxb@gmail.com>
2023-06-10 06:52:32 +0600

    Basic Setup Installed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-3xl text-center text-green-600'>Basic router setup</h1>
+     
+    </>
+  )
+}
+
+export default App
