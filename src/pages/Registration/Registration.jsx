bdda281d7bfd581b2619f2c72ca1ac6522731261
Mizanur Rahman <mizanrxb@gmail.com>
2023-06-12 03:58:05 +0600

    footer added

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -1,12 +1,18 @@
 import { useContext, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { updateProfile } from "firebase/auth";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 
 const Registration = () => {
   const [error, setError] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
-  const { createUser } = useContext(AuthContext);
+  const { createUser, googleSignIn } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
   const handleSignUp = (event) => {
     event.preventDefault();
@@ -17,9 +23,9 @@ const Registration = () => {
     const photoUrl = form.photourl.value;
 
     setError("");
-    if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{6,}$/.test(password)) {
+    if (password.length < 6 || !/[A-Z]/.test(password) || !/[^A-Za-z0-9]/.test(password)) {
       setError(
-        "Your password must be at least 6 characters long and contain both letters and numbers."
+        "Your password must be at least 6 characters long, contain at least one capital letter, and one special character."
       );
       return;
     }
@@ -55,81 +61,105 @@ const Registration = () => {
       });
   };
 
+  const handleGoogleSignIn = () => {
+    googleSignIn()
+      .then((result) => {
+        const loggedInUser = result.user;
+        navigate(from, { replace: true });
+        console.log(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div>
       <div className="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
         <div className="max-w-lg mx-auto">
-          <form
-            onSubmit={handleSignUp}
-            action=""
-            className="p-4 mt-6 mb-0 space-y-8 rounded-lg shadow-lg sm:p-6 lg:p-8"
-          >
-            <p className="text-lg font-medium text-center">
-              Create your account
-            </p>
+          <div className="rounded-lg shadow-lg">
+            <form
+              onSubmit={handleSignUp}
+              action=""
+              className="p-4 mt-6 mb-0 space-y-8 sm:p-6 lg:p-8"
+            >
+              <p className="text-lg font-medium text-center">
+                Create your account
+              </p>
 
-            <div>
-              <input
-                name="name"
-                type="text"
-                className="input-style"
-                placeholder="Enter name"
-              />
+              <div>
+                <input
+                  name="name"
+                  type="text"
+                  className="input-style"
+                  placeholder="Enter name"
+                />
+              </div>
+
+              <div>
+                <input
+                  name="email"
+                  type="email"
+                  className="input-style"
+                  placeholder="Enter email"
+                  required
+                />
+              </div>
+
+              <div>
+                <input
+                  name="password"
+                  type="password"
+                  className="input-style"
+                  placeholder="Enter password"
+                  required
+                />
+              </div>
+
+              <div>
+                <input
+                  name="confirmPassword"
+                  type="password"
+                  className="input-style"
+                  placeholder="Confirm password"
+                  value={confirmPassword}
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div>
+                <input
+                  name="photourl"
+                  type="text"
+                  className="input-style"
+                  placeholder="Enter Photo Url"
+                />
+              </div>
+
+              <button type="submit" className="login-btn">
+                Sign Up
+              </button>
+            </form>
+            <div className="px-8 pt-0 pb-6">
+              <div className="flex flex-col gap-6">
+                <button
+                  onClick={handleGoogleSignIn}
+                  className="flex items-center justify-center w-full px-5 py-3 mx-auto mb-2 mr-2 font-medium text-white bg-red-500 rounded shadow-sm hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                >
+                  <FontAwesomeIcon icon={faGoogle} className="mr-2" />
+                  Sign in with Google
+                </button>
+              </div>
+              <p className="text-sm text-center text-gray-500">
+                Have an account?
+                <Link className="underline" to={"/login"}>
+                  Sign in
+                </Link>
+              </p>
+              <p className="text-sm text-center text-red-600">{error}</p>
             </div>
-
-            <div>
-              <input
-                name="email"
-                type="email"
-                className="input-style"
-                placeholder="Enter email"
-                required
-              />
-            </div>
-
-            <div>
-              <input
-                name="password"
-                type="password"
-                className="input-style"
-                placeholder="Enter password"
-                required
-              />
-            </div>
-
-            <div>
-              <input
-                name="confirmPassword"
-                type="password"
-                className="input-style"
-                placeholder="Confirm password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                required
-              />
-            </div>
-
-            <div>
-              <input
-                name="photourl"
-                type="text"
-                className="input-style"
-                placeholder="Enter Photo Url"
-              />
-            </div>
-
-            <button type="submit" className="login-btn">
-              Sign Up
-            </button>
-
-            <p className="text-sm text-center text-gray-500">
-              Have an account?
-              <Link className="underline" to={"/login"}>
-                Sign in
-              </Link>
-            </p>
-            <p className="text-sm text-center text-red-600">{error}</p>
-          </form>
+          </div>
         </div>
       </div>
     </div>
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,202 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import yogayuq from "../../../assets/hemp-logo_727906-14 (2).png";
+import {
+  faTwitter,
+  faFacebookSquare,
+  faDribbble,
+  faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="relative pt-8 pb-6 my-container bg-blueGray-200">
+      <div className="container px-4 mx-auto">
+        <div className="flex flex-wrap text-left lg:text-left">
+          <div className="w-full px-4 lg:w-4/12">
+            <Link to="/" className="items-center flex-1 inline-block">
+              <img className="h-8" src={yogayuq} alt="" />
+              <span className="text-2xl font-bold tracking-wide text-gray-800">
+                Toy Fusion
+              </span>
+            </Link>
+            <h5 className="mt-0 mb-2 text-lg text-blueGray-600">
+              Where Imagination Meets Action
+            </h5>
+          </div>
+          <div className="w-full px-4 lg:w-4/12">
+            <div className="flex flex-wrap mb-6 items-top">
+              <div className="w-full px-4 lg:w-4/12">
+                <span className="block mb-2 text-sm font-semibold text-red-600 uppercase">
+                  Resources
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a
+                      className="block pb-2 text-sm font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href="#"
+                    >
+                      Terms & Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block pb-2 text-sm font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      FAQ
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block pb-2 text-sm font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Return Policy
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block pb-2 text-sm font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Home Delivery
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="w-full px-4 lg:w-4/12">
+                <span className="block mb-2 text-sm font-semibold text-red-600 uppercase">
+                  Courses
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a
+                      className="block pb-2 text-sm font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Yoga Fundamentals
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block pb-2 text-sm font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Advanced Asanas
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block pb-2 text-sm font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Meditation Techniques
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block pb-2 text-sm font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Yoga for Stress Relief
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+              <div className="w-full px-4 lg:w-4/12">
+                <span className="block mb-2 text-sm font-semibold text-red-600 uppercase">
+                  Your Path to Wellness
+                </span>
+                <ul className="list-unstyled">
+                  <li>
+                    <a
+                      className="block pb-2 text-sm italic font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Health Articles
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block pb-2 text-sm italic font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Nutritional Tips
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      className="block pb-2 text-sm italic font-semibold text-blueGray-600 hover:text-blueGray-800"
+                      href=""
+                    >
+                      Mindfulness Resources
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="w-full px-4 lg:w-4/12">
+            <h1 className="block mb-2 text-sm font-semibold text-center text-red-600 uppercase">
+              Find Us on Social Media
+            </h1>
+            <div className="mt-6 mb-6 text-center lg:mb-0">
+              <button
+                className="items-center justify-center w-10 h-10 mr-2 font-bold bg-white rounded-full shadow-lg outline-none text-lightBlue-400 align-center focus:outline-none"
+                type="button"
+              >
+                <FontAwesomeIcon icon={faTwitter} />
+              </button>
+              <button
+                className="items-center justify-center w-10 h-10 mr-2 font-bold bg-white rounded-full shadow-lg outline-none text-lightBlue-600 align-center focus:outline-none"
+                type="button"
+              >
+                <FontAwesomeIcon icon={faFacebookSquare} />
+              </button>
+              <button
+                className="items-center justify-center w-10 h-10 mr-2 font-bold text-pink-400 bg-white rounded-full shadow-lg outline-none align-center focus:outline-none"
+                type="button"
+              >
+                <FontAwesomeIcon icon={faDribbble} />
+              </button>
+              <button
+                className="items-center justify-center w-10 h-10 mr-2 font-bold bg-white rounded-full shadow-lg outline-none text-blueGray-800 align-center focus:outline-none"
+                type="button"
+              >
+                <FontAwesomeIcon icon={faGithub} />
+              </button>
+            </div>
+          </div>
+        </div>
+        <hr className="my-6 border-blueGray-300" />
+        <div className="flex flex-wrap items-center justify-center md:justify-between">
+          <div className="w-full px-4 mx-auto text-center md:w-4/12">
+            <div className="py-1 text-sm font-semibold text-blueGray-500">
+              Copyright ©{" "}
+              <span id="get-current-year">{new Date().getFullYear()}</span>
+              <a
+                href=""
+                className="text-blueGray-500 hover:text-gray-800"
+                target="blank"
+              >
+                {" "}
+                Toy Fusion by
+              </a>
+              <br />
+              <a href="" className="text-blueGray-500 hover:text-blueGray-800">
+                Mizanur Rahman
+              </a>
+              .
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
